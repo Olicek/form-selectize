b5d024b905110c93799e3e1a980b394a353848c3
Petr Olišar <petr.olisar@gmail.com>
2015-09-03 16:21:31 +0200

    Add custom options

--- client-side/selectize.js
+++ client-side/selectize.js
@@ -1,39 +1,54 @@
 
-function selectize(callback, selector)
+function selectize(callback, selector, customOptions)
 {
-	try {
-		if (selector === undefined) {
-			selector = $('.selectize');
-		}
-		
-		$( selector ).each(function() {
-			var item = $(this);
-			if (item.data('options').mode === 'full') {
-				
-				var valueField = item.data('options').valueField;
-				var labelField = item.data('options').labelField;
-				var options = {
-					plugins: (item.data('options').plugins === null ? null : item.data('options').plugins),
-					delimiter: item.data('options').delimiter,
-					maxItems: item.data('options').maxItems,
-					valueField: valueField,
-					labelField: labelField,
-					searchField: item.data('options').searchField,
-					options: item.data('entity'),
-					create: (item.data('options').create ? true : false)
-				};
-				
-				if (callback !== undefined) {
-					options.render = callback(labelField, valueField);
-				}
-				
-				item.selectize(options);
-			} else {
-				item.selectize({
-					sortField: 'text',
-					create: (item.data('options').create ? true : false)
-				});
+	if ($.fn.selectize === undefined) {
+		console.error('Plugin "selectize.js" is missing! Run `bower install selectize` and load it.');
+		return;
+	}
+
+	if (selector === undefined) {
+		selector = $('.selectize');
+	}
+
+	$( selector ).each(function() {
+		var item = $(this);
+		if (item.data('options').mode === 'full') {
+
+			var valueField = item.data('options').valueField;
+			var labelField = item.data('options').labelField;
+			var options = {
+				plugins: (item.data('options').plugins === null ? null : item.data('options').plugins),
+				delimiter: item.data('options').delimiter,
+				maxItems: item.data('options').maxItems,
+				valueField: valueField,
+				labelField: labelField,
+				searchField: item.data('options').searchField,
+				options: item.data('entity'),
+				create: (item.data('options').create ? true : false),
+			};
+
+			if (callback !== undefined) {
+				options.render = callback(labelField, valueField);
 			}
-		});
-	} catch(err) { console.log('missing selectize!'); }
+
+			if (customOptions !== undefined)
+			{
+				options = customOptions(options);
+			}
+
+			item.selectize(options);
+		} else {
+			var options = {
+				sortField: 'text',
+				create: (item.data('options').create ? true : false)
+			};
+
+			if (customOptions !== undefined)
+			{
+				options = customOptions(options);
+			}
+
+			item.selectize(options);
+		}
+	});
 }
